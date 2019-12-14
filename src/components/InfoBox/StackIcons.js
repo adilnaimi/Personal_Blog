33af7d661186be811811e1da3e4b9c0841a009e2
Pranav Shridhar <pranavmodx@gmail.com>
2019-12-15 00:51:12 +0530

    added avatar, remove stack icons

--- src/components/InfoBox/StackIcons.js
+++ src/components/InfoBox/StackIcons.js
@@ -2,17 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import injectSheet from "react-jss";
 
-import AlgoliaIcon from "!svg-react-loader!../../images/svg-icons/algolia.svg?name=AlgoliaIcon";
-import ReactIcon from "!svg-react-loader!../../images/svg-icons/react.svg?name=ReactIcon";
-import GraphqlIcon from "!svg-react-loader!../../images/svg-icons/graphql.svg?name=GraphqlIcon";
-import JssIcon from "!svg-react-loader!../../images/svg-icons/jss.svg?name=JssIcon";
-import MaterialUiIcon from "!svg-react-loader!../../images/svg-icons/material-ui.svg?name=MaterialUiIcon";
-import ReduxIcon from "!svg-react-loader!../../images/svg-icons/redux.svg?name=ReduxIcon";
-import GatsbyIcon from "!svg-react-loader!../../images/svg-icons/gatsby.svg?name=GatsbyIcon";
-import WebpackIcon from "!svg-react-loader!../../images/svg-icons/webpack.svg?name=WebpackIcon";
-import BabelIcon from "!svg-react-loader!../../images/svg-icons/babel.svg?name=BabelIcon";
-import NetlifyIcon from "!svg-react-loader!../../images/svg-icons/netlify.svg?name=NetlifyIcon";
-
 const styles = theme => ({
   stack: {
     display: "none",
@@ -52,21 +41,11 @@ const StackIcons = props => {
   const { classes } = props;
 
   const items = [
-    { name: "gatsby", url: "https://www.gatsbyjs.org/", comp: GatsbyIcon },
-    { name: "react", url: "https://reactjs.org/", comp: ReactIcon },
-    { name: "graphql", url: "http://graphql.org/", comp: GraphqlIcon },
-    { name: "jss", url: "http://cssinjs.org/", comp: JssIcon },
-    { name: "material-ui", url: "https://material-ui-next.com/", comp: MaterialUiIcon },
-    { name: "redux", url: "https://redux.js.org/", comp: ReduxIcon },
-    { name: "algolia", url: "https://www.algolia.com/", comp: AlgoliaIcon },
-    { name: "webpack", url: "https://webpack.js.org/", comp: WebpackIcon },
-    { name: "babel", url: "https://babeljs.io/", comp: BabelIcon },
-    { name: "netlify", url: "https://www.netlify.com/", comp: NetlifyIcon }
   ];
 
   return (
     <div className={classes.stack}>
-      <h5 className={classes.header}>built with:</h5>
+      {/* <h5 className={classes.header}>built with:</h5> */}
       <div className={classes.box}>
         {items.map(item => {
           const Icon = item.comp;
